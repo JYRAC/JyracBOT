e3190166ca4e3e0b7c2a87e9feb0be341cc469ac
Pitaya <yanamasa1109@gmail.com>
2026-07-29 00:56:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const PUBLIC_COMMANDS = ['receive-notifications', 'request', 'help'];
 
 /** モーダルを表示するコマンド（deferReply不可） */
-const MODAL_COMMANDS = ['notice', 'broadcast', 'request'];
+const MODAL_COMMANDS = ['notice', 'broadcast', 'request', 'entry-message'];
 
 /** オーナー専用コマンド（grant-access / revoke-access / list-access） */
 const OWNER_COMMANDS = ['grant-access', 'revoke-access', 'list-access'];
